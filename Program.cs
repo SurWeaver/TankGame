4a2b504974702efbd6a3fa71c8731b03d833b049
SurWeaver <xander.tka4enko@gmail.com>
2021-01-15 13:10:04 +0300

    Создан класс целого числа для ограничения целочисленных свойств нулем и установленным максимумом.

--- Program.cs
+++ Program.cs
@@ -6,10 +6,53 @@ using System.Threading.Tasks;
 
 namespace TankGame
 {
+
+	
+	
+	class NaturalNumber
+	{
+		int Num;
+		int MaxNum;
+
+		public void Add(int num)
+		{
+			Num = (num + Num).Clamp(0, MaxNum);
+		}
+		public bool IsMax() => Num == MaxNum;
+		public bool IsZero() => Num == 0;
+
+		public NaturalNumber(int num, int maxNum)
+		{
+			Num = num;
+			MaxNum = maxNum;
+		}
+		public NaturalNumber(int num) : this(num, num)
+		{ }
+
+		public static implicit operator int(NaturalNumber n)
+		{
+			return n.Num;
+		}
+	}
+
+
+
+
+	public static partial class ExtensionMethods
+	{
+		public static int Clamp(this int value, int min, int max)
+		{
+			return (value < min) ? min : (value > max) ? max : value;
+		}
+	}
+
 	class Program
 	{
+		
+
 		static void Main(string[] args)
 		{
+			Console.WriteLine()
 		}
 	}
 }
